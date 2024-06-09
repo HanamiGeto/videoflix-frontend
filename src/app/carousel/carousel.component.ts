2f038c1aa091705d26b21be9215b5ce92edf4694
HanamiGeto <tidu11@hotmail.de>
2024-06-09 22:28:36 +0200

    added swiper library and changed styling

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import Swiper from 'swiper/bundle';
 
 @Component({
   selector: 'vf-carousel',
   standalone: true,
   imports: [],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.scss'
+  styleUrl: './carousel.component.scss',
 })
-export class CarouselComponent {
+export class CarouselComponent implements AfterViewInit {
+  @ViewChild('swiper') swiper!: ElementRef;
 
+  ngAfterViewInit(): void {
+    this.initSwiper();
+  }
+
+  private initSwiper() {
+    return new Swiper(this.swiper.nativeElement, {
+      slidesPerView: 6,
+      slidesPerGroup: 6,
+      loop: true,
+      spaceBetween: 5,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+    })
+  }
 }
