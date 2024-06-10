84770b4cd2418d8df1931f5b77005c2cbd211fe9
HanamiGeto <tidu11@hotmail.de>
2024-06-10 19:41:39 +0200

    added footer, dataprotection and imprint component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'vf-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,13 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { BrowseComponent } from './browse/browse.component';
+import { DataprotectionComponent } from './dataprotection/dataprotection.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
 export const routes: Routes = [
   { path: '', component: BrowseComponent },
   { path: 'login', component: LoginComponent },
   { path: 'registration', component: RegistrationComponent },
+  { path: 'data-protection', component: DataprotectionComponent},
+  { path: 'imprint', component: ImprintComponent},
 ];
