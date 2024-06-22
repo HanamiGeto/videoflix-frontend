611d287b8c1cff5c9177d558849dabb1aaab9244
HanamiGeto <tidu11@hotmail.de>
2024-06-22 11:16:27 +0200

    implemented functions for auth service

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,19 +1,34 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private http = inject(HttpClient);
+  private apiUrl = environment.apiUrl;
 
-  register(userData: { username: string; email: string; password: string }) {
-    // return this.http.post(`${this.baseURL}/register`, { ...userData });
+  signup(userData: { username: string; email: string; password: string }) {
     console.log(userData);
+    return this.http.post(`${this.apiUrl}accounts/signup/`, userData);
   }
 
   login(userData: { email: string; password: string }) {
-    // return this.http.post(`${this.baseURL}/register`, { ...userData });
     console.log(userData);
+    return this.http.post(`${this.apiUrl}accounts/login/`, userData).pipe(
+      tap((result) => {
+        localStorage.setItem('authUser', JSON.stringify(result));
+      }),
+    );
+  }
+
+  logout() {
+    localStorage.removeItem('authUser');
+  }
+
+  isLoggedIn() {
+    return localStorage.getItem('authUser') !== null;
   }
 }
