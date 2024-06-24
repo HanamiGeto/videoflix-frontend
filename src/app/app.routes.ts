06e63642f5147539daee47da8517eaa7f141065a
HanamiGeto <tidu11@hotmail.de>
2024-06-24 21:25:43 +0200

    created video upload component and added it to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { BrowseComponent } from './browse/browse.component';
 import { DataprotectionComponent } from './dataprotection/dataprotection.component';
 import { ImprintComponent } from './imprint/imprint.component';
 import { authGuard } from './shared/auth.guard';
+import { VideoUploadComponent } from './video-upload/video-upload.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'browse', component: BrowseComponent, canActivate: [authGuard] },
+  { path: 'upload', component: VideoUploadComponent, canActivate: [authGuard] },
   { path: 'data-protection', component: DataprotectionComponent },
   { path: 'imprint', component: ImprintComponent },
 ];
---
+++ src/app/video-upload/video-upload.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vf-video-upload',
+  standalone: true,
+  imports: [],
+  templateUrl: './video-upload.component.html',
+  styleUrl: './video-upload.component.scss',
+})
+export class VideoUploadComponent {}
