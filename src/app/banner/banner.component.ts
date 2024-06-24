b40813c077bfd05e01cefceb714b8f5d1b8dfb05
HanamiGeto <tidu11@hotmail.de>
2024-06-24 21:25:17 +0200

    updated banner to play video from backend

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
+import { VideoService } from '../shared/video.service';
+import { environment } from '../../environments/environment';
+import { AsyncPipe } from '@angular/common';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'vf-banner',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss',
 })
-export class BannerComponent {}
+export class BannerComponent {
+  videoService = inject(VideoService);
+  id = input(24);
+  apiUrl = environment.apiUrl.slice(0, -1);
+
+  video$ = toObservable(this.id).pipe(
+    switchMap((id) => this.videoService.getSingle(id)),
+  );
+}
