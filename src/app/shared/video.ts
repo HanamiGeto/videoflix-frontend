92ccb90a3e1ae973567e19ad8d207e932ac53b4d
HanamiGeto <tidu11@hotmail.de>
2024-06-27 18:37:54 +0200

    added new property to Video interface and created VideoResolutions interface

--- src/app/shared/video.ts
+++ src/app/shared/video.ts
@@ -5,4 +5,12 @@ export interface Video {
   description: string;
   genre: 'nature' | 'animals' | 'animated';
   video_file: string;
+  video_file_resolutions: VideoResolutions;
+  thumbnail_file: string;
+}
+
+export interface VideoResolutions {
+  _1080p: string;
+  _720p: string;
+  _360p: string;
 }
