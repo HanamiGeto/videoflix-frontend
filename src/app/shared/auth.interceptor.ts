7b570387e674158ec1b227320e8a1c527d2f9a5f
HanamiGeto <tidu11@hotmail.de>
2024-07-07 15:19:36 +0200

    feat: enhance authInterceptor to handle token refresh on expiration

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,18 +1,45 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AuthService } from './auth.service';
+import { catchError, switchMap, throwError } from 'rxjs';
+import { TokenService } from './token.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
-  const token = localStorage.getItem('access_token');
+  const tokenService = inject(TokenService);
+  const token = tokenService.getAccessToken();
+  const reqWithToken = (token: string | null) => {
+    return token
+      ? req.clone({
+          setHeaders: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+      : req;
+  };
 
-  if (authService.isAuthenticated()) {
-    const reqWithToken = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return next(reqWithToken);
+  const handle401TokenError = (error: HttpErrorResponse) => {
+    return authService.refreshToken().pipe(
+      switchMap((tokens) => {
+        const newToken = tokens.access;
+        return next(reqWithToken(newToken));
+      }),
+      catchError(() => {
+        authService.logout();
+        return throwError(() => new Error(error.message));
+      }),
+    );
+  };
+
+  if (authService.isLoggedIn() && token) {
+    return next(reqWithToken(token)).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401 && error.error.code === 'token_not_valid') {
+          return handle401TokenError(error);
+        }
+        return throwError(() => new Error(error.message));
+      }),
+    );
   } else {
     return next(req);
   }
