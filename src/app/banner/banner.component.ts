7870db548ca8216866bc1516588d72cc90a400b3
HanamiGeto <tidu11@hotmail.de>
2024-08-02 19:01:39 +0200

    refactor: replace native video element with vjs-player component

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -6,17 +6,18 @@ import { Observable, switchMap } from 'rxjs';
 import { VideoUrlPipe } from '../shared/video-url.pipe';
 import { Video } from '../shared/video';
 import { RouterLink } from '@angular/router';
+import { VjsPlayerComponent } from '../vjs-player/vjs-player.component';
 
 @Component({
   selector: 'vf-banner',
   standalone: true,
-  imports: [AsyncPipe, VideoUrlPipe, RouterLink],
+  imports: [AsyncPipe, VideoUrlPipe, RouterLink, VjsPlayerComponent],
   templateUrl: './banner.component.html',
   styleUrl: './banner.component.scss',
 })
 export class BannerComponent {
   videoService = inject(VideoService);
-  id = input(4);
+  id = input(6);
 
   video$: Observable<Video> = toObservable(this.id).pipe(
     switchMap((id) => this.videoService.getSingle(id)),
