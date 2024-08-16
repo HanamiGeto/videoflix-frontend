dfbfe8a960f9e2b2b8126fb52ef32df989f8b835
HanamiGeto <tidu11@hotmail.de>
2024-08-16 15:54:31 +0200

    feat: add input properties to preview modal component for dynamic data binding

--- src/app/preview-modal/preview-modal.component.ts
+++ src/app/preview-modal/preview-modal.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { previewModalAnimation } from '../shared/animations';
+import { Video } from '../shared/video';
+import { AnimationEvent } from '@angular/animations';
+import { NgStyle } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'vf-preview-modal',
   standalone: true,
-  imports: [],
+  imports: [NgStyle, RouterLink],
   templateUrl: './preview-modal.component.html',
   styleUrl: './preview-modal.component.scss',
-  animations: [],
+  animations: [previewModalAnimation],
 })
-export class PreviewModalComponent {}
+export class PreviewModalComponent {
+  video = input.required<Video>();
+  startXOffset = input.required<number>();
+  startYOffset = input.required<number>();
+  startScaleX = input.required<number>();
+  startScaleY = input.required<number>();
+  currentStylesContainer = input.required<Record<string, string>>();
+  currentStylesContent = input.required<Record<string, string>>();
+  isPreviewVisible = false;
+
+  onAnimationEvent(event: AnimationEvent): void {
+    if (event.phaseName === 'done') {
+      this.isPreviewVisible = !this.isPreviewVisible;
+    }
+  }
+}
