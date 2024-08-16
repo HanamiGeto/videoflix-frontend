02b01b0a293c077dc1ccc1e3ab60e8f8f4aaee09
HanamiGeto <tidu11@hotmail.de>
2024-08-16 15:48:41 +0200

    feat: create preview modal component for video preview modal

---
+++ src/app/preview-modal/preview-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vf-preview-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './preview-modal.component.html',
+  styleUrl: './preview-modal.component.scss',
+  animations: [],
+})
+export class PreviewModalComponent {}
