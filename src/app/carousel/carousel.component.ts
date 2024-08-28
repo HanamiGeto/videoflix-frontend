4d3dd96b6be9ee9a5f02375486c1709f2b6f249a
HanamiGeto <tidu11@hotmail.de>
2024-08-28 21:38:11 +0200

    refactor: update carousel component to use new video-gallery component

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,23 +1,12 @@
-import {
-  Component,
-  ElementRef,
-  effect,
-  inject,
-  input,
-  signal,
-  viewChild,
-  viewChildren,
-} from '@angular/core';
-import Swiper from 'swiper/bundle';
+import { Component, inject } from '@angular/core';
 import { PreviewModalComponent } from '../preview-modal/preview-modal.component';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { VideoService } from '../shared/video.service';
 import { VideoUrlPipe } from '../shared/video-url.pipe';
-import { Observable, of, switchMap } from 'rxjs';
-import { Video } from '../shared/video';
+import { map } from 'rxjs';
 import { AsyncPipe, NgStyle } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import { VideoPreviewService } from '../shared/video-preview.service';
+import { VideoGalleryComponent } from '../video-gallery/video-gallery.component';
 
 @Component({
   selector: 'vf-carousel',
@@ -28,96 +17,24 @@ import { VideoPreviewService } from '../shared/video-preview.service';
     AsyncPipe,
     RouterLink,
     NgStyle,
+    VideoGalleryComponent,
   ],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.scss',
 })
 export class CarouselComponent {
-  private readonly swiper = viewChild<ElementRef>('swiper');
-  private videoCards = viewChildren<ElementRef>('video');
-  showPreviewOnHover = signal<boolean>(false);
-  videoGenre = input.required<string>();
-  private readonly videoPreviewService = inject(VideoPreviewService);
-  private moduloBase = 6;
-
-  video$: Observable<Video> = this.videoPreviewService.video$;
-
-  videos = toSignal(
+  videoGenres = toSignal(
     inject(VideoService)
       .getAll()
       .pipe(
-        switchMap((videos) =>
-          of(
-            videos
-              .filter((video) => video.genre_display === this.videoGenre())
-              .sort((a, b) => a.id - b.id),
-          ),
-        ),
+        map((videos) => {
+          const genres = Array.from(
+            new Set(videos.map((video) => video.genre_display)),
+          );
+          return genres;
+        }),
       ),
   );
 
-  constructor() {
-    effect(() => {
-      if (this.videos()) {
-        this.initializeSwiper();
-      }
-    });
-  }
-
-  initializeSwiper(): void {
-    if (this.swiper()) {
-      new Swiper(this.swiper()!.nativeElement, {
-        slidesPerView: 6,
-        slidesPerGroup: 6,
-        loop: true,
-        spaceBetween: 6,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          type: 'bullets',
-        },
-      });
-    }
-  }
-
-  displayPreviewModal(videoId: number, index: number): void {
-    this.videoPreviewService.displayPreviewModal(
-      videoId,
-      index,
-      this.videoCards(),
-      this.showPreviewOnHover,
-      this.moduloBase,
-    );
-  }
-
-  clearTime() {
-    this.videoPreviewService.clearTime();
-  }
-
-  get currentStylesContainer() {
-    return this.videoPreviewService.currentStylesContainer();
-  }
-
-  get currentStylesContent() {
-    return this.videoPreviewService.currentStylesContent();
-  }
-
-  get startXOffset() {
-    return this.videoPreviewService.startXOffset();
-  }
-
-  get startYOffset() {
-    return this.videoPreviewService.startYOffset();
-  }
-
-  get startScaleX() {
-    return this.videoPreviewService.startScaleX();
-  }
-
-  get startScaleY() {
-    return this.videoPreviewService.startScaleY();
-  }
+  videos = toSignal(inject(VideoService).getAll());
 }
