c4360eb4be2cb20425b7aaac389ad2d7e5cb38ae
HanamiGeto <tidu11@hotmail.de>
2024-08-28 21:42:54 +0200

    refactor: removed obsolete code after refactoring with new video-gallery component

--- src/app/browse/browse.component.ts
+++ src/app/browse/browse.component.ts
@@ -1,12 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { BannerComponent } from '../banner/banner.component';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { FooterComponent } from '../footer/footer.component';
-import { VideoService } from '../shared/video.service';
-import { map } from 'rxjs';
-import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'vf-browse',
@@ -21,17 +18,4 @@ import { toSignal } from '@angular/core/rxjs-interop';
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss',
 })
-export class BrowseComponent {
-  videoGenres = toSignal(
-    inject(VideoService)
-      .getAll()
-      .pipe(
-        map((videos) => {
-          const genres = Array.from(
-            new Set(videos.map((video) => video.genre_display)),
-          );
-          return genres;
-        }),
-      ),
-  );
-}
+export class BrowseComponent {}
