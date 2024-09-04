4b12e2f72241c2243d2b560379126e6d825a96b9
HanamiGeto <tidu11@hotmail.de>
2024-09-04 21:56:57 +0200

    feat: integrate toast component into the main application template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ChildrenOutletContexts, RouterOutlet } from '@angular/router';
 import { routeTransition } from './shared/animations';
+import { ToastComponent } from './toast/toast.component';
 
 @Component({
   selector: 'vf-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ToastComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   animations: [routeTransition],
