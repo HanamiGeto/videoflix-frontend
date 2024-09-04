d9f994e56e141aad6c4ba750fc967f0dc19c2911
HanamiGeto <tidu11@hotmail.de>
2024-09-04 21:56:34 +0200

    feat: add toast component for displaying toast notifications

---
+++ src/app/toast/toast.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { toastAnimation } from '../shared/animations';
+import { ToastMessage, ToastService } from '../shared/toast.service';
+
+@Component({
+  selector: 'vf-toast',
+  standalone: true,
+  imports: [],
+  templateUrl: './toast.component.html',
+  styleUrl: './toast.component.scss',
+  animations: [toastAnimation],
+})
+export class ToastComponent implements OnInit {
+  private toastService = inject(ToastService);
+  toast: ToastMessage | null = null;
+
+  ngOnInit(): void {
+    this.toastService.toast$.subscribe((toast) => {
+      this.toast = toast;
+    });
+  }
+
+  removeToast() {
+    this.toastService.clearToast();
+  }
+}
