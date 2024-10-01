a64dbe9836fc7eaed5b7141d4aafd41b0afc4a92
HanamiGeto <tidu11@hotmail.de>
2024-10-01 19:18:53 +0200

    feat(user-menu): implement hover logic

--- src/app/user-menu/user-menu.component.ts
+++ src/app/user-menu/user-menu.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
+import { userMenuAnimation } from '../shared/animations';
 
 @Component({
   selector: 'vf-user-menu',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './user-menu.component.html',
   styleUrl: './user-menu.component.scss',
+  animations: [userMenuAnimation],
 })
-export class UserMenuComponent {}
+export class UserMenuComponent {
+  userMenuActive = signal(false);
+  private debounceTimeout?: ReturnType<typeof setTimeout>;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.userMenuActive.set(true);
+  }
+  @HostListener('mouseleave') onMouseLeave() {
+    this.debounceTimeout = setTimeout(() => {
+      this.userMenuActive.set(false);
+    }, 250);
+  }
+
+  clearTime() {
+    clearTimeout(this.debounceTimeout);
+  }
+}
