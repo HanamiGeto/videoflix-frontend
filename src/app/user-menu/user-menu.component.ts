368bf15ec21559512b92b2417919f27e6156fd20
HanamiGeto <tidu11@hotmail.de>
2024-10-01 19:07:04 +0200

    feat: add user menu component for header

---
+++ src/app/user-menu/user-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vf-user-menu',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-menu.component.html',
+  styleUrl: './user-menu.component.scss',
+})
+export class UserMenuComponent {}
