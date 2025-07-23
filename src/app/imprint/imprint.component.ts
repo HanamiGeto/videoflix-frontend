03149e3d14042d00e24f52b7e301a2ba9e6eb09e
HanamiGeto <tidu11@hotmail.de>
2025-07-23 16:35:02 +0200

    feat(imprint): add content and styling

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Location } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'vf-imprint',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss',
 })
-export class ImprintComponent {}
+export class ImprintComponent {
+  private location = inject(Location);
+
+  backClicked() {
+    this.location.back();
+  }
+}
