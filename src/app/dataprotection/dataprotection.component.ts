8147ffd6bbe6ac513f67f6f88ce983d3a84bb54d
HanamiGeto <tidu11@hotmail.de>
2025-07-24 11:56:17 +0200

    feat(data-protection): add content and styling

--- src/app/dataprotection/dataprotection.component.ts
+++ src/app/dataprotection/dataprotection.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Location, ViewportScroller } from '@angular/common';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'vf-dataprotection',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent],
   templateUrl: './dataprotection.component.html',
   styleUrl: './dataprotection.component.scss',
 })
-export class DataprotectionComponent {}
+export class DataprotectionComponent {
+  private location = inject(Location);
+  private viewportScroller = inject(ViewportScroller);
+
+  backClicked() {
+    this.location.back();
+  }
+
+  scrollTo(event: Event, fragment: string) {
+    event.preventDefault();
+    this.viewportScroller.scrollToAnchor(fragment);
+  }
+}
